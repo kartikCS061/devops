b7a0bd3c8c0b26ee69da67c126409f18a22d4036
shwetha_ai <shwethakc959@gmail.com>
2025-04-23 12:46:17 +0530

    added

---
+++ src/main/java/com/jakin/app/App.java
@@ -0,0 +1,13 @@
+package com.jakin.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
